2c7926c2936d98c65c24865a8befe30de8b0935b
cezklosowski <cez.klosowski@gmail.com>
2020-09-05 11:07:14 +0200

    connect Employee class to sql table

--- src/main/java/pl/sda/dto/Employee.java
+++ src/main/java/pl/sda/dto/Employee.java
@@ -1,13 +1,25 @@
 package pl.sda.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Employee {
+
+    @Id
+    @Column(name="id")
     private Long id;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="position")
     private String position;
+    @Column(name="salary")
     private Integer salary;
+    @Column(name="birth_year")
     private Integer birthYear;
 
     public Employee() {
---
+++ src/main/resources/employees_user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee(
+id BIGINT NOT NULL,
+first_name VARCHAR(15),
+last_name VARCHAR(15),
+position VARCHAR(15),
+salary DOUBLE,
+birth_year INT,
+PRIMARY KEY(id)
+);
